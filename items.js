19acd60d5036ee4d070e9247bd523a1c44a969ec
lmarlow2 <47681639+lmarlow2@users.noreply.github.com>
2019-05-11 16:16:34 -0500

    Update items.js

--- items.js
+++ items.js
@@ -188,15 +188,15 @@ var weaponProperties = [ "Increase the damage die from d4 to d6. You can select 
                     "(Melee only.) You use your choice of your Strength or Dexterity modifier for the attack and damage rolls. You must use the same modifier for both rolls. Optional Limitations: This property cannot be combined with the Heavy or Versatile properties. This property cannot be combined with the brutal property when applied to a simple weapon.",
                     "(Martial only.) Increase the damage die from a d4 to a d8, a d6 to a d10 or a d8 to d12. This weapon gains the Two-handed property if it didn't have it already. Small creatures have disadvantage on attack rolls with this weapon. Limitation: This property cannot increase a ranged weapon's damage dice to a d12.",
                     "(Melee only.) This weapon is ideal for two-weapon fighting. This cannot be combined with the heavy property. Limitation: This property can only be applied to weapons with a damage die of d4 or d6. This property can't be combined with the versatile property",
+                    "(Ranged only.) Remove the Two-handed property. Limitation: This counts as two properties for a simple ranged weapon.",
                     "(Ranged only.) Remove the Loading property.",
+                    "(Martial melee only.) Your reach increase by 5 feet when using the weapon. Limitation: This counts as two properties unless the weapon has the Heavy property.",
                     "(Ranged only.) Increase the range of a simple ranged weapon to 80/320. Increase the range of a martial ranged weapon to 150/600.",
                     "(Melee only.) You can throw this weapon to make a ranged attack with a range of 20/60. You use the same ability modifier for that attack roll and damage roll that you would use for a melee attack with the weapon. You can select this property again to increase the range to 30/120. Limitation: This can only be applied to weapons with a damage die of d4 or d6.",
-                    "(Melee only.) This weapon can be used with one or two hands. Increase the damage die for melee attacks when wielded with two hands from a d4 to a d6, from a d6 to a d8, or from a d8 to a d10. This cannot be combined with the Heavy property.",
-                    "(Martial melee only.) Your reach increase by 5 feet when using the weapon. Limitation: This counts as two properties unless the weapon has the Heavy property.",
-                    "(Ranged only.) Remove the Two-handed property. Limitation: This counts as two properties for a simple ranged weapon."
+                    "(Melee only.) This weapon can be used with one or two hands. Increase the damage die for melee attacks when wielded with two hands from a d4 to a d6, from a d6 to a d8, or from a d8 to a d10. This cannot be combined with the Heavy property."
 ];
 
-var weaponPropertyList = ["Brutal", "Finesse", "Heavy", "Light", "Rapid-Fire", "Sniping", "Thrown", "Versatile", "Reach", "One-Handed"];
+var weaponPropertyList = ["Brutal", "Finesse", "Heavy", "Light", "One-Handed", "Rapid-Fire", "Reach", "Sniping", "Thrown", "Versatile"];
 
 function updateInfoBox(id){
   var select = document.getElementById(id);
@@ -239,77 +239,7 @@ function forgeWeapon(){
   var allowedProperty = [ True, isMelee, !isSimple, isMelee, !isMelee, !isMelee, isMelee && !isSimple, !isMelee, isMelee, isMelee ];
   var pickedProperties = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ];
   var properties = [ document.getElementById("propertyOne").selectedIndex, document.getElementById("propertyTwo").selectedIndex, document.getElementById("propertyThree").selectedIndex ];
-  for(var i = 0; i < numProperties; ++i){
-    if(!allowedProperty[properties[i]]){
-      alert("The combination of properties you have selected is not allowed. Please double check your item properties.");
-      return;
-    }
-    switch(properties[i]){
-      case 0: //brutal
-        if(++pickedProperties[0] > 1 || (isSimple && isMelee)) allowedProperty[0] = False;
-        if(isSimple){ 
-          allowedProperty[1] = False;
-          if(pickedProperties[1]){ 
-            alert("Cannot combine the Brutal and Finesse properties on a Simple weapon!");
-            return;
-          }
-        }
-        damageDie += 2;
-        break;
-      case 1: //Finesse
-        if(isSimple){
-          allowedProperty[0] = False;
-          if(pickedProperties[0]){ 
-            alert("Cannot combine the Brutal and Finesse properties on a Simple weapon!");
-            return;
-          }
-        }
-        allowedProperty[2] = False;
-        allowedProperty[9] = False;
-        if(pickedProperties[2] || pickedProperties[9]){
-          alert("Cannot combine the Finesse property with either the Heavy property or the Versatile property!");
-          return;
-        }
-        allowedProperty[1] = False;
-        pickedProperties[1]++;
-        break;
-      case 2: //Heavy
-        if(isMelee || damageDie < 10)
-          damageDie += 2;
-        allowedProperty[2] = False;
-        pickedProperties[2]++;
-        isTwoHanded = True;
-        break;
-      case 3: //Light only if damageDie is <= 6
-        allowedProperty[2] = False;
-        allowedProperty[9] = False;
-        if(pickedProperties[2] || pickedProperties[9] || damageDie > 6){
-          alert("Cannot combine Light property with either the Heavy property or the Versatile property. The Light Property can only be applied to weapons that deal below 1d6 damage.");
-          return;
-        }
-        allowedProperty[3] = False;
-        pickedProperties[3]++;
-        break;
-      case 4: //Rapid-Fire
-        isReloaded = False;
-        pickedProperties[4]++;
-        allowedProperty[4] = False;
-        break;
-      case 5: //Sniping
-        break;
-      case 6: //Thrown
-        break;
-      case 7: //Versatile
-        break;
-      case 8: //Reach counts as two unless Heavy
-        break;
-      case 9: //One-Handed counts as 2 properties if simple
-        if(isSimple) numProperties--;
-        pickedProperties[9]++;
-        allowedProperty[9] = False;
-        break
-    }
-  }
+  for(var i = 0; i < 3; i++) pickedProperties[properties[i]]++;
   document.getElementById("weaponName").innerHTML = document.getElementById("nameWeapon").value;
   document.getElementById("weaponClass").innerHTML = (isSimple ? "Simple " : "Martial ") + (isMelee ? "Melee " : "Ranged ") + "Weapon";
   document.getElementById("weaponDamage").innerHTML = "1d" + damageDie;
